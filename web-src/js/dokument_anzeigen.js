55e475a2cd75d2169c927f101aa93b72a20dcd99
ehoppe92 <elisabethhoppe@web.de>
2014-06-11 11:10:01 +0200

    iwas

---
+++ web-src/js/dokument_anzeigen.js
@@ -0,0 +1,61 @@
+var handler = handler || {};
+
+handler.bindDokAnzeigenEvents = function() {
+	//Dokument bearbeiten
+	$("#aDokBearbeiten").click(function(){
+		alert("Dok bearbeiten was clicked");
+		//get the dokid 
+		var dokid = $("dokInhalt").data("dokId");
+		
+		//Dok id ans href anhängen
+		$("#aDokBearbeiten").attr("href", "DokumentBearbeiten.html?id="+dokid);
+	});
+	
+	
+};
+
+handler.loadDok = function() {
+	console.log("Im loadDoc");
+	//get the id of the dok
+	var query = window.location.search.substring(1);
+	var parts = query.split("=");
+	var id = parseInt(parts[1]);
+	console.log(id);
+	
+	//ajax - Abfrage, um dieses Dokument aus der DB zu lesen
+	jQuery.ajax({
+		type: 'GET',
+		url: "http://localhost:8080/einstieg2014/rest/DocumentStorage/getDocument/"+id,
+		
+		contentType: "application/json",
+		
+		
+			}).done(function(data) {
+				console.log(data);
+				
+				/*var output = '';
+				for (key in data) {
+			        output += '<li>' + data[key].firstName + '</li>';
+			    }
+				
+				
+				$('#searchPat').append(output).trigger('create');*/
+				
+				$('#DokName').val(data.filename);
+				$('#docContent').val(data.content);
+				
+				//DokId speichern
+				$("#dokInhalt").data("dokId", id);
+				
+			}).fail(function(error) {
+				
+			});
+	
+	
+}
+
+
+$(document).ready(function(){
+	handler.bindDokAnzeigenEvents();
+	handler.loadDok();
+});
